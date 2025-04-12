b28870cf967129f82753760a0be2fc42fd017ace
domingueroA <youtumundialdominguero@gmail.com>
2025-04-12 13:00:50 -0500

    Update ContactUs.jsx

--- src/components/ContactUs/ContactUs.jsx
+++ src/components/ContactUs/ContactUs.jsx
@@ -5,8 +5,8 @@ const ContactUs = () => {
     return (
         <div className="contact-us-container">
             <h2>Contact Us</h2>
-            <p>Email: <a href="mailto:contact@neevaa.co.uk">contact@neevaa.co.uk</a></p>
-            <p>Phone: <a href="tel:0601413055">0601413055</a></p>
+            <p>Email: <a href="mailto:contact@quickrecipeskitchen.com">contact@quickrecipeskitchen.com</a></p>
+            <p>Phone: <a href="tel:+51995487759">+51995487759</a></p>
         </div>
     );
 };
