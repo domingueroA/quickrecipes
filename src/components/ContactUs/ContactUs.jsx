d9c1e2feffd1598f4a0592bf675dad47af6eb703
rosita2025 <196705675+rosita2025@users.noreply.github.com>
2025-04-12 17:30:58 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/components/ContactUs/ContactUs.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './ContactUs.css';
+
+const ContactUs = () => {
+    return (
+        <div className="contact-us-container">
+            <h2>Contact Us</h2>
+            <p>Email: <a href="mailto:contact@neevaa.co.uk">contact@neevaa.co.uk</a></p>
+            <p>Phone: <a href="tel:0601413055">0601413055</a></p>
+        </div>
+    );
+};
+
+export default ContactUs;
